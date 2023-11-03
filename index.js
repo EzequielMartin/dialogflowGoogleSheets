efc573c73837e59c8dc926b21400944d7b3404b4
Ezequiel Martin <ezequielmartin92@gmail.com>
2023-11-02 22:08:23 -0300

    Agrego funcion para encuesta del GIDAS

--- index.js
+++ index.js
@@ -1,47 +1,40 @@
 const express = require('express');
-const app = express();
 const { WebhookClient } = require("dialogflow-fulfillment");
 const {google} = require("googleapis");
 
+const app = express();
+
 app.get('/', function (req, res) {
-  res.send('Webhook Online, Ya se puede usar desde Dialogflow');
+  res.send('<script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script><df-messenger intent="WELCOME" chat-title="Pruebas" agent-id="0d1be57d-e009-4424-8a7f-496d4eceb7c3" language-code="es"></df-messenger>');
 });
 
 app.post('/webhook',express.json() ,function (req, res) {
 
     const agent = new WebhookClient({ request: req, response: res });
-    //console.log('Dialogflow Request headers: ' + JSON.stringify(req.headers));
-    //console.log('Dialogflow Request body: ' + JSON.stringify(req.body));
     
-    function welcome(agent) {
-        agent.add(`Welcome to my agent!`);
-    };
+    // function welcome(agent) {
+    //     agent.add(`Welcome to my agent!`);
+    // };
     
-    function fallback(agent) {
-        agent.add(`I didn't understand`);
-        agent.add(`I'm sorry, can you try again?`);
-    };
+    // function fallback(agent) {
+    //     agent.add(`I didn't understand`);
+    //     agent.add(`I'm sorry, can you try again?`);
+    // };
 
-    const auth = new google.auth.GoogleAuth({
-        
-        keyFile: "credentials.json",
-        scopes: "https://www.googleapis.com/auth/spreadsheets"
+    function encuestaTest(agent){
 
-    });
-
-    //Create client instance for auth
-    const client = auth.getClient();
-
-    //Create Instance of Google Sheets API
-    const googleSheets = google.sheets({
-        
-        version: "v4",
-        auth: client
-    });
-
-    const spreadsheetId = "1ewlyWHWESZdumyPc_Hiiq6VJNzkZNHqqpYOX--fkd5w";
-
-    function encuesta(agent){
+        const auth = new google.auth.GoogleAuth({
+            keyFile: "credentials.json",
+            scopes: "https://www.googleapis.com/auth/spreadsheets"
+        });
+        //Create client instance for auth
+        const client = auth.getClient();
+        //Create Instance of Google Sheets API
+        const googleSheets = google.sheets({
+            version: "v4",
+            auth: client
+        });
+        const spreadsheetId = "1ewlyWHWESZdumyPc_Hiiq6VJNzkZNHqqpYOX--fkd5w";
 
         console.log(agent.parameters);
 
@@ -66,11 +59,58 @@ app.post('/webhook',express.json() ,function (req, res) {
         
         agent.end("");
     };
+
+    function encuestaGidas(agent){
+
+        const auth = new google.auth.GoogleAuth({
+            keyFile: "credentials.json",
+            scopes: "https://www.googleapis.com/auth/spreadsheets"
+        });
+        const client = auth.getClient();
+        const googleSheets = google.sheets({
+            version: "v4",
+            auth: client
+        });
+        const spreadsheetId = "1bnLRBVfI-2tC40e75L-t76Hs-Eqj4rxcnlke9fQNkBw";
+
+        console.log(agent.parameters);
+
+        const nombre = agent.parameters.nombre;
+        const apellido = agent.parameters.apellido;
+        const area = agent.parameters.area.join(",\n\n");
+        const areaSugerencia = agent.parameters.areaSugerencia;
+        const disciplina = agent.parameters.disciplina.join(",\n\n");
+        const disciplinaSugerencia = agent.parameters.disciplinaSugerencia;
+        const proyecto = agent.parameters.proyecto.join(",\n\n");
+        const proyectoSugerencia = agent.parameters.proyectoSugerencia;
+        const actividad = agent.parameters.actividad.join(",\n\n");
+        const actividadSugerencia = agent.parameters.actividadSugerencia;
+        const email = agent.parameters.email;
+        const celular = agent.parameters.celular;
+        const anio = agent.parameters.anio;
+        const motivacion = agent.parameters.motivacion.join(",\n\n");
+        const comoConociste = agent.parameters.comoConociste.join(",\n\n");
+
+        googleSheets.spreadsheets.values.append({
+            auth,
+            spreadsheetId,
+            range: "encuesta",
+            valueInputOption: "USER_ENTERED",
+            resource: {
+                values: [
+                    [nombre, apellido, area, areaSugerencia, disciplina, disciplinaSugerencia, proyecto, proyectoSugerencia, actividad, actividadSugerencia, email, celular, anio, motivacion, comoConociste]
+                ]
+            }
+        });
+
+        agent.end("");
+    };
     
     let intentMap = new Map();
-    intentMap.set('Default Welcome Intent', welcome);
-    intentMap.set('Default Fallback Intent', fallback);
-    intentMap.set('encuesta', encuesta);
+    // intentMap.set('Default Welcome Intent', welcome);
+    // intentMap.set('Default Fallback Intent', fallback);
+    intentMap.set('encuestaTest', encuestaTest);
+    intentMap.set('encuestaGidas', encuestaGidas);
     agent.handleRequest(intentMap);
     });
 
